328e7981e659b2cb1cc4b7f50ba8b0bec50abfe1
Eric Kenney <e.kenney@contactelevation.com>
2020-08-19 09:13:56 -0600

    Use reactivesearch on home

--- src/app/Home/Home.tsx
+++ src/app/Home/Home.tsx
@@ -1,12 +1,29 @@
 import React from 'react'
 import LogoutButton from 'app/common/LogoutButton'
+import {
+  ReactiveBase,
+  DataSearch,
+  ReactiveList,
+} from '@appbaseio/reactivesearch'
 
 const Home = (): JSX.Element => {
-  return <div>
-    Hello sweet world. Please give me purpose...
-    <br/>
-    <LogoutButton />
-  </div>
+  return (
+    <ReactiveBase app='listings' url='http://localhost:9200/'>
+      <DataSearch
+        componentId='SearchSensor'
+        dataField={ ['title', 'description'] }
+      />
+      <ReactiveList
+        componentId='SearchResult'
+        react={ {
+          and: ['SearchSensor'],
+        } }
+        renderItem={ (res) => <div key={ res.id }>{res.title}</div> }
+        dataField='title'
+      />
+      <LogoutButton />
+    </ReactiveBase>
+  )
 }
 
 export default Home
