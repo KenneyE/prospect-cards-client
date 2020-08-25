99d3d46aa7debe2f5c8d4ee1e5b41296f7b7005d
Eric Kenney <e.kenney@contactelevation.com>
2020-08-24 21:17:27 -0600

    Show error message

--- src/app/account/AddPayment/AddPayment.tsx
+++ src/app/account/AddPayment/AddPayment.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { AddPaymentQuery } from 'types/graphql'
 import { Button } from '@material-ui/core'
 import { loadStripe } from '@stripe/stripe-js'
+import { toast } from 'react-toastify'
+
 import ErrorMessage from 'app/common/ErrorMessage'
 
 interface Props {
@@ -18,13 +20,14 @@ const AddPayment = ({
     // When the customer clicks on the button, redirect them to Checkout.
     const stripe = await stripePromise
 
-    if (!stripe) return <ErrorMessage/>
+    if (!stripe) return <ErrorMessage />
     const { error } = await stripe.redirectToCheckout({
       sessionId: stripeCheckoutSessionId,
     })
-    // If `redirectToCheckout` fails due to a browser or network
-    // error, display the localized error message to your customer
-    // using `error.message`.
+
+    if (error) {
+      toast.error(error.message)
+    }
   }
 
   return <Button onClick={ handleClick }>Add Payment Method</Button>
