6fd205aac24a7f7c9c69f13a1613056e3daa52da
Eric Kenney <e.kenney@contactelevation.com>
2020-10-01 15:49:18 -0600

    Unused components'

--- src/app/Login/Login.tsx
+++ src/app/Login/Login.tsx
@@ -1,6 +1,5 @@
 import React, { useState, SyntheticEvent } from 'react'
 import { RouteComponentProps } from 'react-router'
-import Button from '@material-ui/core/Button'
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import CardMedia from '@material-ui/core/CardMedia'
