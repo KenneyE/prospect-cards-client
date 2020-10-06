26880b6fc48974aa5d33bf76f252e73457ea5d61
Eric Kenney <e.kenney@contactelevation.com>
2020-10-06 14:42:06 -0600

    Make confirmation slide

--- src/app/common/Confirmation/Confirmation.tsx
+++ src/app/common/Confirmation/Confirmation.tsx
@@ -6,20 +6,30 @@ import {
   DialogContent,
   DialogContentText,
   DialogTitle,
+  Slide,
 } from '@material-ui/core'
 import { confirmable, ReactConfirmProps } from 'react-confirm'
 import theme from 'lib/theme'
 import { MuiThemeProvider } from '@material-ui/core/styles'
+import { TransitionProps } from '@material-ui/core/transitions'
 
 interface Props extends ReactConfirmProps {
   text: string;
 }
+
+const Transition = React.forwardRef(function Transition(
+  props: TransitionProps & { children?: React.ReactElement<any, any> },
+  ref: React.Ref<unknown>,
+) {
+  return <Slide direction='up' ref={ ref } { ...props } />
+})
 
 const Confirmation = ({ show, proceed, text }: Props): JSX.Element => {
   return (
     <MuiThemeProvider theme={ theme }>
       <Dialog
         open={ show }
+        TransitionComponent={ Transition }
         onClose={ () => proceed() }
         aria-labelledby='alert-dialog-title'
         aria-describedby='alert-dialog-description'
