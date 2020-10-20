572ff37f2e0c6fd8359288a4b72859e809ae01e9
Eric Kenney <e.kenney@contactelevation.com>
2020-10-19 19:29:46 -0600

    Show no current offers message

--- src/app/MyOffers/MyOffers.tsx
+++ src/app/MyOffers/MyOffers.tsx
@@ -17,30 +17,34 @@ const MyOffers = ({
   return (
     <>
       <Typography>Offers You've Placed</Typography>
-      {offers.map(
-        (offer): JSX.Element => (
-          <Card key={ offer.id } className={ classes.card }>
-            <div className={ classes.details }>
-              <CardContent>
-                <Typography gutterBottom variant='body1'>
-                  {offer.listing.title}
-                </Typography>
-                <Typography gutterBottom variant='body1'>
-                  {centsToDollars(offer.price)}
-                </Typography>
-              </CardContent>
-            </div>
-            <div className={ classes.imgWrapper }>
-              <CardMedia
-                className={ classes.img }
-                component='img'
-                alt={ offer.listing.title }
-                image={ offer.listing.images[0].url }
-                title={ offer.listing.title }
-              />
-            </div>
-          </Card>
-        ),
+      {offers.length ? (
+        offers.map(
+          (offer): JSX.Element => (
+            <Card key={ offer.id } className={ classes.card }>
+              <div className={ classes.details }>
+                <CardContent>
+                  <Typography gutterBottom variant='body1'>
+                    {offer.listing.title}
+                  </Typography>
+                  <Typography gutterBottom variant='body1'>
+                    {centsToDollars(offer.price)}
+                  </Typography>
+                </CardContent>
+              </div>
+              <div className={ classes.imgWrapper }>
+                <CardMedia
+                  className={ classes.img }
+                  component='img'
+                  alt={ offer.listing.title }
+                  image={ offer.listing.images[0].url }
+                  title={ offer.listing.title }
+                />
+              </div>
+            </Card>
+          ),
+        )
+      ) : (
+        <Typography variant='body2'>You have no current offers</Typography>
       )}
     </>
   )
