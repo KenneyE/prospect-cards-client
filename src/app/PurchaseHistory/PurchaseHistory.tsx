6e933c83d78df7d35db9301c2e9c25f178bdb6b5
Eric Kenney <e.kenney@contactelevation.com>
2020-11-16 22:28:14 -0700

    No purchases message

--- src/app/PurchaseHistory/PurchaseHistory.tsx
+++ src/app/PurchaseHistory/PurchaseHistory.tsx
@@ -20,19 +20,24 @@ const PurchaseHistory = ({
 
   return (
     <>
-      {purchases.map((purchase) => (
-        <Paper key={ purchase.id } className={ classes.purchase }>
-          <Image image={ purchase.listing.images[0] } className={ classes.img } />
-          <Typography>{purchase.listing.title}</Typography>
+      {purchases.length ?
+        purchases.map((purchase) => (
+          <Paper key={ purchase.id } className={ classes.purchase }>
+            <Image
+              image={ purchase.listing.images[0] }
+              className={ classes.img }
+            />
+            <Typography>{purchase.listing.title}</Typography>
 
-          <Typography>{centsToDollars(purchase.offer.price)}</Typography>
-          <Typography>{purchase.listing.seller.username}</Typography>
-          <Typography>{dateFormat(purchase.createdAt)}</Typography>
-          <Fab color='primary'>
-            <ReceiptIcon />
-          </Fab>
-        </Paper>
-      ))}
+            <Typography>{centsToDollars(purchase.offer.price)}</Typography>
+            <Typography>{purchase.listing.seller.username}</Typography>
+            <Typography>{dateFormat(purchase.createdAt)}</Typography>
+            <Fab color='primary'>
+              <ReceiptIcon />
+            </Fab>
+          </Paper>
+        )) :
+        'You have no previous purchases'}
     </>
   )
 }
